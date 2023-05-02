7348673a6ec44ff6da01e8caaba7d880322757b4
makospace <1505548152@qq.com>
2023-05-02 19:03:26 +0800

    reference

--- code/src/include/Exception.h
+++ code/src/include/Exception.h
@@ -1,13 +1,3 @@
-/**
- * @file Exception.h
- * @author 冯岳松 (yuesong-feng@foxmail.com)
- * @brief
- * @version 0.1
- * @date 2022-02-07
- *
- * @copyright Copyright (冯岳松) 2022
- *
- */
 #pragma once
 #include <iostream>
 #include <stdexcept>
--- code/src/include/SignalHandler.h
+++ code/src/include/SignalHandler.h
@@ -1,13 +1,3 @@
-/**
- * @file Signal.h
- * @author 冯岳松 (yuesong-feng@foxmail.com)
- * @brief
- * @version 0.1
- * @date 2022-02-07
- *
- * @copyright Copyright (冯岳松) 2022
- *
- */
 #pragma once
 #include <signal.h>
 #include <functional>
